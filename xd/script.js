5eb5bc0eb1be4eeca5dc3c61cbdb8323292ec3fe
Dan <dangamer076@gmail.com>
2024-02-12 23:31:22 -0600

    Initial commit

---
+++ xd/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+    var envelope = $("#envelope");
+    var openButton = $("#open");
+    var resetButton = $("#reset");
+
+    openButton.click(function () {
+        envelope.removeClass("close").addClass("open");
+        // Agregar el mensaje "Feliz Día de San Valentín" al abrir la carta
+        $(".line1").text("Feliz");
+        $(".line2").text("Día de San Valentín");
+        $(".line3").text("");
+        $(".line4").text("");
+    });
+
+    resetButton.click(function () {
+        envelope.removeClass("open").addClass("close");
+        $(".line1, .line2, .line3, .line4").text("");
+    });
+});
