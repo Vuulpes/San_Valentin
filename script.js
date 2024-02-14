175b6eef577f9c1d1208d6712ed8649aa64b712e
Dan <dangamer076@gmail.com>
2024-02-14 00:58:31 -0600

    sanvalentin

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ $(document).ready(function () {
     openButton.click(function () {
         envelope.removeClass("close").addClass("open");
         // Agregar el mensaje "Feliz Día de San Valentín" al abrir la carta
-        $(".line1").text("Feliz");
-        $(".line2").text("Día de San Valentín");
+        $(".line1").text("Feliz Día de San Valentín");
+        $(".line2").text("Y Feliz Cumpleaños Amiga Gera");
         $(".line3").text("");
         $(".line4").text("");
     });
